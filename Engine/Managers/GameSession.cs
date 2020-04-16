7c8430d90b2fee96c32eb1d580e246ae8deaa613
HThing4 <hdog121@gmx.com>
2020-04-16 16:02:31 -0400

    Reached C4.Created and can display a play class along with world. Got images. Rewrote by adding a Object initializer. Finished Ch1 of C# in Depth

---
+++ Engine/Managers/GameSession.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Engine.Classes;
+
+namespace Engine.Managers
+{
+   public class GameSession
+    {
+        public Player currentPlayer { get; set; }
+
+        public GameSession()
+        {
+            currentPlayer = new Player
+            {
+                Name = "Player",
+                Gold = 100000,
+                CharacterClass = "Paladin",
+                Level = 1,
+                HitPoints = 10,
+                ExperiencePoints = 0
+            };
+
+        }
+    }
+}
